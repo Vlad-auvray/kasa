b68570bbb81fad0f6f340f22ee30583f9d4df2db
Vlad <vladimir.auvray@gmail.com>
2023-01-09 10:54:56 +0100

    Update footer
    ajout du scrollToTop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import logo from "../../images/logo.svg";
 export default function Footer() {
 	return (
 		<footer className="footer-block">
-			<Link to="/" className="toHome" >
+			<Link to="/" onClick="window.scrollTo(0, 0);" className="toHome" >
 			<img src={logo} alt="logo Kasa" />
 			</Link>
 			<p className="footer-block__text">© 2020 Kasa. All rights reserved</p>
